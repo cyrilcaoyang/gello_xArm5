0f999c04dc9c8cefbc54c84dcddaa14a3e75569e
Yang Cao <yangcyril.cao@utoronto.ca>
2025-08-10 19:13:57 -0400

    Update launch_nodes.py with latest changes

--- experiments/launch_nodes.py
+++ experiments/launch_nodes.py
@@ -17,51 +17,43 @@ class Args:
 
 def launch_robot_server(args: Args):
     port = args.robot_port
+    MENAGERIE_ROOT: Path = (
+        Path(__file__).parent.parent / "third_party" / "mujoco_menagerie"
+    )
+    
     if args.robot == "sim_ur":
-        MENAGERIE_ROOT: Path = (
-            Path(__file__).parent.parent / "third_party" / "mujoco_menagerie"
-        )
         xml = MENAGERIE_ROOT / "universal_robots_ur5e" / "ur5e.xml"
         gripper_xml = MENAGERIE_ROOT / "robotiq_2f85" / "2f85.xml"
         from gello.robots.sim_robot import MujocoRobotServer
 
         server = MujocoRobotServer(
-            xml_path=xml, gripper_xml_path=gripper_xml, port=port, host=args.hostname
+            xml_path=str(xml), gripper_xml_path=str(gripper_xml), port=port, host=args.hostname
         )
         server.serve()
     elif args.robot == "sim_yam":
-        MENAGERIE_ROOT: Path = (
-            Path(__file__).parent.parent / "third_party" / "mujoco_menagerie"
-        )
         xml = MENAGERIE_ROOT / "i2rt_yam" / "yam.xml"
         from gello.robots.sim_robot import MujocoRobotServer
 
         server = MujocoRobotServer(
-            xml_path=xml, gripper_xml_path=None, port=port, host=args.hostname
+            xml_path=str(xml), gripper_xml_path=None, port=port, host=args.hostname
         )
         server.serve()
     elif args.robot == "sim_panda":
         from gello.robots.sim_robot import MujocoRobotServer
 
-        MENAGERIE_ROOT: Path = (
-            Path(__file__).parent.parent / "third_party" / "mujoco_menagerie"
-        )
         xml = MENAGERIE_ROOT / "franka_emika_panda" / "panda.xml"
         gripper_xml = None
         server = MujocoRobotServer(
-            xml_path=xml, gripper_xml_path=gripper_xml, port=port, host=args.hostname
+            xml_path=str(xml), gripper_xml_path=str(gripper_xml), port=port, host=args.hostname
         )
         server.serve()
     elif args.robot == "sim_xarm":
         from gello.robots.sim_robot import MujocoRobotServer
 
-        MENAGERIE_ROOT: Path = (
-            Path(__file__).parent.parent / "third_party" / "mujoco_menagerie"
-        )
         xml = MENAGERIE_ROOT / "ufactory_xarm7" / "xarm7.xml"
         gripper_xml = None
         server = MujocoRobotServer(
-            xml_path=xml, gripper_xml_path=gripper_xml, port=port, host=args.hostname
+            xml_path=str(xml), gripper_xml_path=str(gripper_xml), port=port, host=args.hostname
         )
         server.serve()
 
